e34abefcfd683669a1a5746e9e6121285e4ce48e
coleridge113 <josemanuelmluna@gmail.com>
2025-11-25 14:50:36 +0800

    added logging to ably.js

--- server/utils/ably.js
+++ server/utils/ably.js
@@ -4,6 +4,14 @@ import Ably from 'ably';
 const client = new Ably.Realtime({ key: process.env.ABLY_API_KEY });
 const channel = client.channels.get("ably-channel");
 
+client.connection.on('connectionstate', (stateChange) => {
+    console.log(
+        '[Ably] connection state:',
+        stateChange.previous, '->', stateChange.current,
+        stateChange.reason ? `reason=${stateChange.reason.message}` : ''
+    );
+});
+
 export function publishMessage(data) {
     channel.publish("ably-route", data);
 }
